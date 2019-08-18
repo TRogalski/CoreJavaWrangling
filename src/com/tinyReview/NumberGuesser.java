70f3a152692901ed903d7d2f64e93d29f52aa9c7
TRogalski <tomracc@gmail.com>
2019-08-18 23:41:02 +0200

    Simple guess a number game to repeat some of the core java syntax

---
+++ src/com/tinyReview/NumberGuesser.java
@@ -0,0 +1,65 @@
+package com.tinyReview;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuesser {
+
+    private static Integer numberToGuess;
+
+    public static void main(String[] args) {
+        NumberGuesser numberGuesser = new NumberGuesser();
+        numberGuesser.playTheGame();
+    }
+
+
+    private void playTheGame() {
+        this.showWelcomeMessages();
+        this.generateANumberToBeGuessed();
+        this.runCoreGameLogic();
+        this.displayExitMessages();
+    }
+
+
+    private void generateANumberToBeGuessed() {
+        Random random = new Random();
+        this.numberToGuess = random.nextInt(101) - 1;
+    }
+
+
+    private void showWelcomeMessages() {
+        System.out.println("This is a guess a number game, the goal is to guess" +
+                " randomly generated integer number from range from 1 to 100");
+    }
+
+
+    private void runCoreGameLogic() {
+        System.out.print("Guess a number between 0 and 100: ");
+
+        Integer guess;
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            try {
+                guess = scanner.nextInt();
+                if (guess == numberToGuess) {
+                    System.out.println("Nice, the number we were looking for was " + numberToGuess);
+                    break;
+                } else if (guess > numberToGuess) {
+                    System.out.print("Your guess is too large, try again: ");
+                } else if (guess < numberToGuess) {
+                    System.out.print("Your guess is too low, try again: ");
+                }
+            } catch (InputMismatchException e) {
+                System.out.print("This is not a number, try again: ");
+                scanner.next();
+            }
+        }
+    }
+
+
+    private void displayExitMessages() {
+        System.out.println("Thank you for playing!");
+    }
+}
